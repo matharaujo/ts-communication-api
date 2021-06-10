4dd9e1e9fd55cf6c51f1c99e234407544651cb99
Mateus Araújo <mathsilvaa20@hotmail.com>
2021-06-09 21:50:59 -0300

    Added validation on communication type

--- src/main/app/controllers/communication-controller.ts
+++ src/main/app/controllers/communication-controller.ts
@@ -59,13 +59,13 @@ export class CommunicationController extends Controller {
             if(!moment(dateSubmission).isValid()) throw new Error('Invalid date for dateSubmission! Verify!')
             if(dateSubmission) dateSubmission = moment(dateSubmission).format('YYYY-MM-DD HH:mm:ss');
 
-            this.communication.dateSubmission = dateSubmission;
-            this.communication.recipient = recipient;
-            this.communication.message = message;
-
             if (!dateSubmission) throw new Error('dateSubmission is obrigatory! Verify!');
             if (!recipient) throw new Error('recipient is obrigatory! Verify!');
             if (!message) throw new Error('message is obrigatory! Verify!');
+
+            this.communication.dateSubmission = dateSubmission;
+            this.communication.recipient = recipient;
+            this.communication.message = message;
 
             const result = await this.communicationService.save(this.communication);
             return this.response.status(201)
@@ -107,13 +107,17 @@ export class CommunicationController extends Controller {
         } = this.request.body as {type: string, approved: boolean};
 
         try {
+            if (!id) throw new Error('id is obrigatory! Verify!');
+            if (!type) throw new Error('type is obrigatory! Verify!');
+            if (!approved) throw new Error('approved is obrigatory! Verify!');
+
+            const acceptedTypes = ['email', 'sms', 'push', 'whatsapp'];
+            if(!acceptedTypes.includes(type)) throw new Error(`Types accepted [${acceptedTypes}]! Verify!`);
+
             this.communication.id = id;
             this.communication.type = type;
             this.communication.approved = approved;
 
-            if (!id) throw new Error('id is obrigatory! Verify!');
-            if (!type) throw new Error('type is obrigatory! Verify!');
-            if (!approved) throw new Error('approved is obrigatory! Verify!');
             if (typeof approved !== 'boolean') throw new Error('approved needs to be boolean! Verify!');
 
             const communication = await this.communicationService.findOneById(id);
